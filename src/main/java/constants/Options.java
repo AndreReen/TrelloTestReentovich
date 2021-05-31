6163fc6b32725799b4ca9f7637762dab77c73020
AndreReen <43380198+AndreReen@users.noreply.github.com>
2021-06-01 01:30:40 +0300

    Initial

--- src/main/java/constants/Options.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public final class Options {
-
-    public static final String FIELD_NAME = "name";
-    public static final String FIELD_URL = "url";
-
-}
--- src/main/java/constants/userTypes.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public class userTypes {
-    public static final String NORMAL = "normal";
-    public static final String ADMIN = "admin";
-    public static final String OBSERVER = "observer";
-}
