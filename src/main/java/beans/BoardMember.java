8781f6d9c4226e01968fd375a544deb9ecfe5dff
AndreReen <43380198+AndreReen@users.noreply.github.com>
2021-06-04 13:16:28 +0300

    Updates 2

---
+++ src/main/java/beans/BoardMember.java
@@ -0,0 +1,88 @@
+
+package beans;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class BoardMember {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("fullName")
+    @Expose
+    private String fullName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(BoardMember.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("username");
+        sb.append('=');
+        sb.append(((this.username == null)?"<null>":this.username));
+        sb.append(',');
+        sb.append("fullName");
+        sb.append('=');
+        sb.append(((this.fullName == null)?"<null>":this.fullName));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.fullName == null)? 0 :this.fullName.hashCode()));
+        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
+        result = ((result* 31)+((this.username == null)? 0 :this.username.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof BoardMember) == false) {
+            return false;
+        }
+        BoardMember rhs = ((BoardMember) other);
+        return ((((this.fullName == rhs.fullName)||((this.fullName!= null)&&this.fullName.equals(rhs.fullName)))&&((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id))))&&((this.username == rhs.username)||((this.username!= null)&&this.username.equals(rhs.username))));
+    }
+
+}
